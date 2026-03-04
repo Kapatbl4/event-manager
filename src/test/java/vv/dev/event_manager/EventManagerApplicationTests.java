0661b32bceba7775178483ee2a9a6ed67d30e07f
Kopatbl4 <vvi93@yandex.ru>
2026-03-04 23:48:51 +0300

    Initial commit

---
+++ src/test/java/vv/dev/event_manager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package vv.dev.event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
